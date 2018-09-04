25bdcd45a9d327414c77bf32ac5b01c20cca97e2
Gene Morrisey Zablan <genemorriseyzablan@gmail.com>
2018-09-04 13:02:22 +0800

    Testing lambda

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
     currentTime.setTimezone("America/Los_Angeles");
-    console.log('Test');
     callback(null, {
         statusCode: '200',
         body: 'The time in Los Angeles is: ' + currentTime.toString(),
